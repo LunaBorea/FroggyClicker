367a7bb87a1bf8eebabcee251a2dc3073a1fbc31
LunaBorea <129622236+LunaBorea@users.noreply.github.com>
2024-11-20 13:04:55 +0200

    Enemies list

--- enemies.js
+++ enemies.js
@@ -1,12 +1,103 @@
 const enemies = [
     {
-        name: 'tadpole',
+        name: 'Tadpole',
         stats: {
-            hp: 10,
+            hp: 20,
             gold: [1, 3]
         },
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
-    }
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+        img: 'images/tadpole.png'
+    },
+    {
+        name: 'Young Frog',
+        stats: {
+            hp: 35,
+            gold: [1, 3]
+        },
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+        img: 'images/youngfrog.png'
+    },
+    {
+        name: 'Adult Frog',
+        stats: {
+            hp: 54,
+            gold: [1, 3]
+        },
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+        img: 'images/adultfrog.png'
+    },
+    {
+        name: 'Squire Frog',
+        stats: {
+            hp: 77,
+            gold: [1, 3]
+        },
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+        img: 'images/squire.png'
+    },
+    {
+        name: 'Knight Frog',
+        stats: {
+            hp: 107,
+            gold: [1, 3]
+        },
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+        img: 'images/knightfrog.png'
+    },
+    {
+        name: 'Baron Frog',
+        stats: {
+            hp: 149,
+            gold: [1, 3]
+        },
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+        img: 'images/baronfrog.png'
+    },
+    {
+        name: 'Count Frog',
+        stats: {
+            hp: 208,
+            gold: [1, 3]
+        },
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+        img: 'images/countfrog.png'
+    },
+    {
+        name: 'Duke Frog',
+        stats: {
+            hp: 295,
+            gold: [1, 3]
+        },
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+        img: 'images/dukefrog.png'
+    },
+    {
+        name: 'King Frog',
+        stats: {
+            hp: 423,
+            gold: [1, 3]
+        },
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+        img: 'images/kingfrog.png'
+    },
+    {
+        name: 'Emperor Frog',
+        stats: {
+            hp: 616,
+            gold: [1, 3]
+        },
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+        img: 'images/emperorfrog.png'
+    },
+    {
+        name: 'Frog God',
+        stats: {
+            hp: 910,
+            gold: [1, 3]
+        },
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+        img: 'images/froggod.png'
+    },
 ]
 
 export default enemies
